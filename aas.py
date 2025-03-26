e5316450427e507eed37dc554607d6cafb6de650
unknown <229214@TECH.WYB.AC.LK>
2025-03-26 20:20:25 +0530

    first commit

---
+++ aas.py
@@ -0,0 +1,14 @@
+num=0
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+max_num = num1  
+
+if num2 > max_num:
+      max_num = num2
+
+if num3 > max_num:
+      max_num = num3
+      print(max_num)
+
